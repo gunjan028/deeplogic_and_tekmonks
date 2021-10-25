285943dea6da990350caf8d7c0780c204bebb131
gunjan028 <86643044+gunjan028@users.noreply.github.com>
2021-10-25 23:15:07 +0530

    Add files via upload
    second commit(regex expression replaced by the split function)

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 var http = require('http');
 var request = require('request')
 var express = require('express');
+
 var app = express();
+
 app.use(express.json());
+
 app.get('/getTimeStories',function(req, res, next){
   var latestNews =[];
   request("https://time.com", function(err,apiResp, body ){
-    var latestSection = (body.split("homepage-module latest")[1]).split("</section>")[0];
-    var titleText = /<a.*<\/a>/g;
-    var titles = latestSection.match(titleText);    
+    var latestSection = (body.split("homepage-module latest")[1]).split("</section>")[0]  
+    // var titleText = /<a.*<\/a>/g;
+    // var titles = latestSection.match(titleText);    
+    var titles = latestSection.split("</a>");
     console.log("getTime stories", titles);
-    for(var i = 0; i< titles.length; i++){
-      var title=titles[i];
-      var link = "https://time.com/" + ((title.split("href=")[1]).split(">")[0]);
-      var header = title.split("/>")[1].split("</a>")[0];
+    for(var i = 0; i< titles.length - 1; i++){
+      var title=titles[i].split("<a href=")[1];
+      console.log("tt", title)
+      var link = "https://time.com" + (title.split(">")[0]);
+      var header = title.split("/>")[1];
       latestNews.push({link, title: header});
     }
     res.send(latestNews)
   })
 });
+
 app.set('port', 8000);
+
 var server = http.createServer(app);
+
 server.listen(8000);
